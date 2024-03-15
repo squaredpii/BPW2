3cc6b5c00b2a5ab20d08c51dd378b6bb6c15fb4b
squaredpii <vermijsemma@gmail.com>
2024-03-15 10:35:42 +0100

    dissolve en undissolve scripts

---
+++ Assets/dissolver.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dissolver : MonoBehaviour
+{
+    public float dissolveDuration = 2;
+    public float dissolveStrength;
+
+
+    public void StartDissolver()
+    {
+        StartCoroutine(dissolveer());
+    }
+    public IEnumerator dissolveer()
+    {
+        float elapsedTime = 0;
+
+        Material dissolveMaterial = GetComponent<Renderer>().material;
+
+        while (elapsedTime < dissolveDuration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            dissolveStrength = Mathf.Lerp(0, 1, elapsedTime / dissolveDuration);
+            dissolveMaterial.SetFloat("_dissolvestrength", dissolveStrength);
+
+            yield return null;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartDissolver();
+        }
+    }
+}
